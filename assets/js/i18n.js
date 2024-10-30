315ffabc370de434b56734df8512d51a457f8757
Pedro Leda <pedro.henrique.leda@gmail.com>
2024-10-29 23:16:08 -0300

    (Feature) incluindo labels no i18n

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,4 +1,3 @@
-// i18n.js
 const i18n = {
   en: {
     aboutMe: "About me",
@@ -18,6 +17,62 @@ const i18n = {
     socialNetworks: "Social Networks",
     aboutMeContent:
       "Mobile developer specializing in iOS and React Native, with additional skills in frontend development. Passionate about crafting intuitive and innovative user experiences. Constantly seeking challenges to expand my skills and contribute to high-impact projects.",
+    academicEducation: "Academic Education",
+    employmentHistory: "Employment History",
+    experience: "Experience",
+    bachelorDegree: "Bachelor in Computer Science",
+    inss: "Instituto Nacional do Seguro Social",
+    techSupportInternship: "Technical Support Internship",
+    userSupportDescription:
+      "User support activities such as machine repairs, software configurations, network configurations like proxies and tokens, hardware repair, formatting, and installation of OS's among other activities.",
+    inepe: "Instituto Nacional de Estudos e Pesquisas",
+    softwareDevIntern: "Software Development Intern",
+    softwareDevDescription:
+      "Worked on the software architects team on small web projects using Node.js for backend operations, Angular (version 1.x) for frontend, and GitLab for teamwork and versioning.",
+    firstDecision: "First Decision",
+    fullStackIntern: "Full Stack Development Intern",
+    fullStackDescription:
+      "Web and Mobile Application Development with JavaScript, using Node.js for backend, and React and React Native for frontend and mobile development. Used MongoDB for database, Git for versioning, and agile methodologies for project management and UX design for usability testing.",
+    bbTech: "BB Tecnologia e Serviços",
+    itAnalyst: "IT Analyst | Junior Developer",
+    itAnalystDescription:
+      "Creation and stylization of web pages, maintenance of microservices, and mobile application development.",
+    useMobile: "UseMobile",
+    iosJuniorDev: "Junior iOS Developer",
+    iosJuniorDevDescription:
+      "Developing applications for the iOS platform using and implementing knowledge such as Clean Architecture, Clean Code, and using Gitflow and MVVM architecture.",
+    compassUol: "Compass Uol",
+    currentIosDev: "Junior iOS Developer",
+    currentIosDevDescription:
+      "Working as a junior iOS Developer. Assisting the mobile team, creating and adjusting features, and fixing bugs.",
+    skillsSection: "Skills",
+    projectsDeveloped: "Developed Projects",
+    tinderClone: "Tinder Clone",
+    tinderDescription:
+      "The app uses static data and main functions like swipe, super like, and like, implemented both by buttons and gestures.",
+    movie4You: "Movie4You",
+    movie4YouDescription:
+      "iOS project that uses the TMDB API to display movies by categories such as upcoming releases and most watched.",
+    sendMessage: "Send Message",
+    locationInfo: "Location",
+    emailInfo: "Email",
+    callInfo: "Call",
+    followYourDreams: "Follow your dreams always making good choices.",
+    contactMe: "Contact me",
+    mainProjects: "Main Projects:",
+    iPadApp: "Dass iPad App for offline inventory requests.",
+    carrefourApp: "Cartão Carrefour app to manage finances.",
+    iguatemiApp: "Iguatemi 365 e-commerce for Iguatemi mall.",
+    frontendDevelopment:
+      "Frontend Development with HTML, CSS, JavaScript, and frameworks like React and Angular.",
+    webDevelopment: "Web Development using JavaScript back-end technologies.",
+    mobile: "Mobile",
+    webServers: "Web Servers",
+    databaseExperience:
+      "Experience with SAP HANA, Power Designer, Sequelize, and MongoDB.",
+    backendTechnologies: "Backend Technologies",
+    developer: "Developer",
+    imA: "I'm a",
   },
   pt: {
     aboutMe: "Sobre mim",
@@ -37,5 +92,62 @@ const i18n = {
     socialNetworks: "Redes Sociais",
     aboutMeContent:
       "Desenvolvedor mobile especializado em iOS e React Native, com habilidades adicionais em desenvolvimento frontend. Apaixonado por criar experiências de usuário intuitivas e inovadoras. Constantemente em busca de desafios para expandir minhas habilidades e contribuir para projetos de alto impacto.",
+    academicEducation: "Formação Acadêmica",
+    employmentHistory: "Histórico Profissional",
+    experience: "Experiência",
+    bachelorDegree: "Bacharel em Ciência da Computação",
+    inss: "Instituto Nacional do Seguro Social",
+    techSupportInternship: "Estágio em Suporte Técnico",
+    userSupportDescription:
+      "Atividades de suporte ao usuário, como reparos de máquinas, configurações de software, configurações de rede como proxies e tokens, reparo de hardware, formatação e instalação de SOs, entre outras atividades.",
+    inepe: "Instituto Nacional de Estudos e Pesquisas",
+    softwareDevIntern: "Estágio em Desenvolvimento de Software",
+    softwareDevDescription:
+      "Trabalhei na equipe de arquitetos de software em pequenos projetos web usando Node.js para operações de backend, Angular (versão 1.x) para frontend e GitLab para trabalho em equipe e versionamento.",
+    firstDecision: "First Decision",
+    fullStackIntern: "Estágio em Desenvolvimento Full Stack",
+    fullStackDescription:
+      "Desenvolvimento de Aplicações Web e Mobile com JavaScript, usando Node.js para backend, React e React Native para frontend e desenvolvimento mobile. Utilizou MongoDB para banco de dados, Git para versionamento e metodologias ágeis para gestão de projetos e UX para testes de usabilidade.",
+    bbTech: "BB Tecnologia e Serviços",
+    itAnalyst: "Analista de TI | Desenvolvedor Júnior",
+    itAnalystDescription:
+      "Criação e estilização de páginas web, manutenção de microsserviços e desenvolvimento de aplicativos móveis.",
+    useMobile: "UseMobile",
+    iosJuniorDev: "Desenvolvedor iOS Júnior",
+    iosJuniorDevDescription:
+      "Desenvolvimento de aplicativos para a plataforma iOS utilizando e implementando conhecimentos como Clean Architecture, Clean Code e utilizando Gitflow e arquitetura MVVM.",
+    compassUol: "Compass Uol",
+    currentIosDev: "Desenvolvedor iOS Júnior",
+    currentIosDevDescription:
+      "Atuando como Desenvolvedor iOS Júnior. Auxiliando a equipe mobile, criando e ajustando funcionalidades e corrigindo bugs.",
+    skillsSection: "Habilidades",
+    projectsDeveloped: "Projetos Desenvolvidos",
+    tinderClone: "Tinder Clone",
+    tinderDescription:
+      "O app utiliza dados estáticos e funções principais como passar, super like e like, implementadas tanto por botões quanto por gestos.",
+    movie4You: "Movie4You",
+    movie4YouDescription:
+      "Projeto para iOS que utiliza a API do TMDB para exibir filmes por categorias como próximos lançamentos e mais assistidos.",
+    sendMessage: "Enviar Mensagem",
+    locationInfo: "Localização",
+    emailInfo: "Email",
+    callInfo: "Telefone",
+    followYourDreams: "Siga seus sonhos sempre fazendo boas escolhas.",
+    contactMe: "Entre em contato",
+    mainProjects: "Principais Projetos:",
+    iPadApp: "Aplicativo para iPad da Dass para pedidos de inventário offline.",
+    carrefourApp: "Aplicativo Cartão Carrefour para gerenciar finanças.",
+    iguatemiApp: "Iguatemi 365 e-commerce para o shopping Iguatemi.",
+    frontendDevelopment:
+      "Desenvolvimento Frontend utilizando HTML, CSS, JavaScript e Frameworks como React e Angular.",
+    webDevelopment:
+      "Desenvolvimento Web utilizando tecnologias de back-end JavaScript.",
+    mobile: "Mobile",
+    webServers: "Servidores Web",
+    databaseExperience:
+      "Experiência com SAP HANA, Power Designer, Sequelize e MongoDB.",
+    backendTechnologies: "Tecnologias de Back-end",
+    developer: "Desenvolvedor(a)",
+    imA: "Eu sou um(a)",
   },
 };
