88e69b44f604aa1220e6fd706b9e7a5a5b2c64ff
Pedro Leda <pedro.henrique.leda@gmail.com>
2025-02-02 17:36:54 -0300

    feature: interacting particles

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
       color: { value: "#ffffff" },
       shape: { type: "circle" },
       opacity: { value: 0.5, random: true },
-      size: { value: 3, random: true },
+      size: { value: 5, random: true },
       line_linked: {
         enable: true,
         distance: 150,
         color: "#ffffff",
-        opacity: 0.4,
+        opacity: 0.5,
         width: 1,
       },
       move: {
@@ -31,8 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
         resize: true,
       },
       modes: {
+        grab: { distance: 200, line_linked: { opacity: 1 } },
         repulse: { distance: 100, duration: 0.4 },
         push: { particles_nb: 4 },
+        remove: { particles_nb: 2 },
       },
     },
     retina_detect: true,
