d1c44f37efa2c30cae57f5855a0fdaa79ca1b65d
Pedro Leda <pedro.henrique.leda@gmail.com>
2025-02-02 17:53:33 -0300

    feature: typed effect.

---
+++ assets/js/typed.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".typed", {
+    strings: ["Frontend", "Mobile", "iOS"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+    showCursor: true,
+  });
+});
